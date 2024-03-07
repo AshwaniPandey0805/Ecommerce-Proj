d431f3762d55c129a91f06141904e9984bbd8603
Ashwani Kumar Pandey <ashwanipandey679@gmail.com>
2024-03-07 10:52:34 +0530

    added user cart product migration file.

---
+++ Ecommerce-Proj/database/migrations/2024_11_14_050559_create_user_cart_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_cart_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('cart_id');
+            $table->string('product_id');
+            $table->integer('wish_list')->default(0)->comment('1 or added, 0 for null ');
+            $table->integer('qunatity');
+            $table->timestamps();
+            $table->foreign('cart_id')->references('card_id')->on('user_carts')->onDelete('cascade');
+            $table->foreign('product_id')->references('sku_number')->on('product_tables')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_cart_products');
+    }
+};
